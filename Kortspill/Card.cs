40a6552a44cae750d482f0eb3f950767aca8c71d
sprtn <jompawars@gmail.com>
2017-03-02 19:36:09 +0100

    Documentation in the Card class.

--- Kortspill/Card.cs
+++ Kortspill/Card.cs
@@ -2,10 +2,28 @@
 {
     public class Card
     {
+        /// <summary>
+        /// The face of the card, i.e. Ace to Kig.
+        /// </summary>
         public string face;
+
+        /// <summary>
+        /// The Suit of the card, i.e. Heart, Diamond, Spades and Clubs.
+        /// </summary>
         public string suit;
+
+        /// <summary>
+        /// The value of the card, ranging from 1 to 13.
+        /// </summary>
         public int value;
 
+        /// <summary>
+        /// The Constructor function of the Card class. 
+        /// Sets the card's variables to the input values.
+        /// </summary>
+        /// <param name="cardFace"></param>
+        /// <param name="cardSuit"></param>
+        /// <param name="cardValue"></param>
         public Card (string cardFace, string cardSuit, int cardValue)
         {
             face = cardFace;
@@ -13,6 +31,11 @@
             value = cardValue;
         }
 
+        /// <summary>
+        /// Overrides the ToString method to something useable for
+        /// us, when checking which card we are pulling out of the deck..
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return face + " of " + suit + " with a value of " + value;
