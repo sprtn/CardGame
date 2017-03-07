c6e0fc0a3d197c5793231088c0f906403115f5fc
jompa <jompa@LAPTOP-QIPQAH98>
2017-03-07 14:55:31 +0100

    Add project files.

---
+++ Kortspill/Card.cs
@@ -0,0 +1,54 @@
+﻿namespace Kortspill
+{
+
+    public class Card
+    {
+        /// <summary>
+        /// The face of the card, i.e. Ace to Kig.
+        /// </summary>
+        public string face;
+
+        /// <summary>
+        /// The Suit of the card, i.e. Heart, Diamond, Spades and Clubs.
+        /// </summary>
+        public string suit;
+
+        /// <summary>
+        /// The value of the card, ranging from 1 to 13.
+        /// </summary>
+        public int value;
+
+        /// <summary>
+        /// The filepath to the card's image
+        /// </summary>
+        public string imgPath;
+
+        /// <summary>
+        /// The Constructor function of the Card class. 
+        /// Sets the card's variables to the input values.
+        /// 
+        /// Also wanted to add a string value where the 
+        /// filepath for the image of the Card was input into
+        /// the constructor.
+        /// </summary>
+        /// <param name="cardFace"></param>
+        /// <param name="cardSuit"></param>
+        /// <param name="cardValue"></param>
+        public Card (string cardFace, string cardSuit, int cardValue)
+        {
+            face = cardFace;
+            suit = cardSuit;
+            value = cardValue;
+        }
+
+        /// <summary>
+        /// Overrides the ToString method to something useable for
+        /// us, when checking which card we are pulling out of the deck..
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return face + " of " + suit + " with a value of " + value;
+        }
+    }
+}
---
+++ Kortspill/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+/// <summary>
+/// TBD:
+/// 
+/// Unit Testing
+/// Bilder for alle kort
+/// </summary>
+
+namespace Kortspill
+
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Deck deck = new Deck();
+        private Card currentCard;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Click event of the Sort Deck button.
+        /// Replaces the current Deck with a new one, 
+        /// and removes the text from the textbox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sortButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Sorts the deck, arranging the cards by value and color. Ace (1) -> King (13).
+            deck.Sort();
+            removeText();
+        }
+
+        /// <summary>
+        /// Click event of the Pull Card button. 
+        /// Deals a card from the deck and calls the writeToCardBox method,
+        /// which writes in the values into the textbox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pullCardButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Pulls out the topmost card and takes it out of the stack
+            currentCard = deck.DealCard();
+            writeToCardBox(currentCard);
+        }
+
+        /// <summary>
+        /// Uses the overwritten ToString method from Card to paste
+        /// information about the card drawn into the TextBox.
+        /// </summary>
+        /// <param name="currentCard"></param>
+        private void writeToCardBox(Card currentCard)
+        {
+            if (currentCard != null)
+            {
+                CardsBox.AppendText(currentCard.ToString() + Environment.NewLine);
+            }
+            else
+            {
+                CardsBox.AppendText("You are out of cards. Please sort or shuffle.");
+                CanPullCards(false);
+            }
+            CardsBox.ScrollToEnd();
+        }
+
+        private void CanPullCards(bool v)
+        {
+            if (pullCardButton.IsEnabled != v)
+                pullCardButton.IsEnabled = v;
+        }
+
+        /// <summary>
+        /// Click event of the shuffle button. Calls the Shuffle function
+        /// from the deck and clears the CardsBox from text.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void shuffleStackButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Code that shuffles the stack, placing the cards randomly
+            deck.Shuffle();
+            removeText();
+        }
+
+        /// <summary>
+        /// Resets the CardsBox textbox, and enables cards to be pulled out of the stack.
+        /// </summary>
+        private void removeText()
+        {
+            CardsBox.Text = null;
+            CanPullCards(true);
+        }
+    }
+}
